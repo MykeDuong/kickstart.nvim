1703cb413ae33b9466552b5e3a5cd9caec506301
mykeduong <hongminh4402@gmail.com>
2026-04-19 10:36:53 -0700

    Add legacy branch for nvim-treesitter

--- init.lua
+++ init.lua
@@ -860,6 +860,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = "master", -- This pins it to the legacy version
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
